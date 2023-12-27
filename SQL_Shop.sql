05f93c396f62293074801df4a0a4b383001d6a41
Huỳnh Ngọc Hiếu Quang <18130188@st.hcmuaf.edu.vn>
2023-12-27 18:24:09 +0700

    thêm dữ liệu lần 2

--- SQL_Shop.sql
+++ SQL_Shop.sql
@@ -1,11 +1,11 @@
-create table ShopCategory(
-	id_ShopCategory bigint auto_increment,
-	image Longblob,
+create table Shop_Category(
+	id_Shop_Category bigint auto_increment,
+	image Nvarchar(80),
     id_Category bigint,
-    shopName nvarchar(255) not null,
+    shop_Name nvarchar(255) not null,
     price nvarchar(255) not null,
     description text,
-    primary key (id_ShopCategory),
+    primary key (id_Shop_Category),
     constraint lien_ket_1 foreign key(id_Category) references Category(id_Category)
 );
 create table Category(
@@ -13,3 +13,11 @@ create table Category(
     Categoryl nvarchar(30) not null,
     primary key(id_Category)
 );
+insert into Category(id_Category,Categoryl) values(1,'Laptop');
+insert into Category(id_Category,Categoryl) values(2,'Máy tính bảng');
+insert into Category(id_Category,Categoryl) values(3,'Điện thoại');
+insert into Category(id_Category,Categoryl) values(4,'Phụ kiện');
+insert into Shop_Category values(1,'iphone15.png',3,'Iphone 15','34000000','Iphone 15 với những cải tiến so với thế hệ tiền nhiệm như: Nút gạt thay bằng bấm, cổng Lightning thay bằng USB type-c');
+insert into Shop_Category values(2,'macbook.png',1,'Macbook Air 2023','40000000','Iphone 15 với những cải tiến so với thế hệ tiền nhiệm như: Nút gạt thay bằng bấm, cổng Lightning thay bằng USB type-c');
+insert into Shop_Category values(3,'airpods.png',4,'Airpods pro 2023','10000000','Iphone 15 với những cải tiến so với thế hệ tiền nhiệm như: Nút gạt thay bằng bấm, cổng Lightning thay bằng USB type-c');
+insert into Shop_Category values(4,'appleWatch.png',4,'Apple Watch 2023','9000000','Iphone 15 với những cải tiến so với thế hệ tiền nhiệm như: Nút gạt thay bằng bấm, cổng Lightning thay bằng USB type-c');
--- project-app/src/Component/Item/ItemNb.jsx
+++ project-app/src/Component/Item/ItemNb.jsx
@@ -1,46 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ItemNb.css'
-import iphone15 from "../Assets/iphone15.jpg"
+
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import macbook from "../Assets/macbook.jpg"
-import airpod from "../Assets/airpods.jpg"
-import appleWatch from "../Assets/appleWatch.jpg"
+
 function ItemNb() {
-    
+      const [images,setImage]=useState([]);
+      useEffect(()=> {
+        fetchData();
+      },[]);
+      const fetchData=async ()=>{
+        try{
+          const response=await fetch(`http://localhost:8080/product`);
+          
+            if(response.ok){
+              const productData=await response.json();
+              setImage(productData);
+            }else{
+              console.error("fail to data")
+            }
+          
+        }catch(error){
+          console.error('eror data',error);
+        }
+      }
       return (
         <div className='ItemNb'>
-            <div>
-              <img src={iphone15} alt=''/>
-              <h3>Iphone 15 Promax</h3>
-              <p>Iphone 15 với những cải tiến so với thế hệ tiền nhiệm
-                như: Nút gạt thay bằng bấm, cổng Lightning thay bằng USB type-c
+          {
+            images.map(image=>(
+              <div key={image.id_Shop_Category}>
+              <img src={`http://localhost:8080/product/getimage/${image.image}`} alt={image.image}/>
+              <h3>{image.shop_Name}</h3>
+              <p>
+                {image.description}
               </p>
               <li className=''>Mua ngay <KeyboardArrowRightIcon style={{fontSize:'medium'}}/></li>
-            </div>
-            <div>
-              <img src={macbook} alt=''/>
-              <h3>Macbook Air 2023</h3>
-              <p>Iphone 15 với những cải tiến so với thế hệ tiền nhiệm
-                như: Nút gạt thay bằng bấm, cổng Lightning thay bằng USB type-c
-              </p>
-              <li className=''>Mua ngay <KeyboardArrowRightIcon style={{fontSize:'medium'}}/></li>
-            </div>
-            <div>
-              <img src={airpod} alt=''/>
-              <h3>Airpod Pro 2023</h3>
-              <p>Iphone 15 với những cải tiến so với thế hệ tiền nhiệm
-                như: Nút gạt thay bằng bấm, cổng Lightning thay bằng USB type-c
-              </p>
-              <li className=''>Mua ngay <KeyboardArrowRightIcon style={{fontSize:'medium'}}/></li>
-            </div>
-            <div>
-              <img src={appleWatch} alt=''/>
-              <h3>Apple Watch SE 2023</h3>
-              <p>Iphone 15 với những cải tiến so với thế hệ tiền nhiệm
-                như: Nút gạt thay bằng bấm, cổng Lightning thay bằng USB type-c
-              </p>
-              <li className=''>Mua ngay <KeyboardArrowRightIcon style={{fontSize:'medium'}}/></li>
-            </div>
+            </div> 
+            ))
+          
+          }
+           
         </div>
       );
     }
